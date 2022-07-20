1f144a02b1e7165f8fc2c88ddebd9df9038e773b
Cayke Daniel <caykedanielcd@gmail.com>
2022-07-20 06:31:03 -0300

    Application complete, connected to firebase and operations working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,29 @@
 import { NavigationContainer } from "@react-navigation/native";
-
+import { useState, useEffect } from "react";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import { SingIn } from "../screens/SingIn";
 import { AppRoutes } from "./app.routes";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((res) => {
+      setUser(res);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SingIn />}
     </NavigationContainer>
   );
 }
--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,16 +1,43 @@
+import auth from "@react-native-firebase/auth";
 import { useState } from "react";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import Logo from "../assets/logo_primary.svg";
 import { Envelope, Key } from "phosphor-react-native";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { Alert } from "react-native";
+
 export function SingIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { colors } = useTheme();
 
   function handleSingIn() {
-    console.log(email, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (
+          error.code === "auth/invalid-email" ||
+          error.code === "auth/wrong-password"
+        ) {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "Usuário não encontrado.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar.");
+      });
   }
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -34,7 +61,12 @@ export function SingIn() {
         secureTextEntry
         onChangeText={setPassword}
       />
-      <Button title="Entrar" w="full" onPress={handleSingIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSingIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
